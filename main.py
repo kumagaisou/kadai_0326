dcf627058a5935981113da86bdec539db781f788
kumagai sou <s.kumagai.sys20@morijyobi.ac.jp>
2021-03-26 16:10:15 +0900

    間違ったコミット

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ print("bmiの計算")
 w = int(input("体重: "))
 h = int(input("身長: "))
 bmi = calc_bmi.bmi_cal(h, w)
-print(f"あなたのbmiは{bmi * 10000}です")
+for i in range(2):
+    print(f"あなたのbmiは{bmi * 10000}です")
